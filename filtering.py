a3ea72d24e4b75b54bdb725e08080fcb168e0fbb
Joao <dev.joaogustavo.dev@gmail.com>
2024-07-10 19:38:45 -0300

    learned about filtering using operators

--- filtering.py
+++ filtering.py
@@ -23,9 +23,51 @@ print(f"The research included {len(dataframe["ESTADO"].unique())} states.") """
 #"dataframe.query(CONDITION)" is a function for FILTERING the results in the dataframe.
 #!This function DO NOT return a BOOLEAN SERIES (It's not a selection, it's the real and ready FILTERED RESULTS).
 #!Same if the query is a string, the COLUMN - which is a dataframe property - cannot be between quotation marks.
-only_SE = dataframe.query("ESTADO == 'SERGIPE'");
+""" only_SE = dataframe.query("ESTADO == 'SERGIPE'"); """
 #Notice that, after the filtering the indexes does not starts at 0.
 #If you want the indexes for starting at 0, use the command ".reset_index()".
 #By the way, if you reset the indexes, a new column containing the original indexes is created.
 #If the created column is not desired, set the "drop" (BOOLEAN) parameter at the ".reset_index()" function. 
-print(only_SE.reset_index(drop=True));
+""" print(only_SE.reset_index(drop=True)); """
+
+#===> FAST CHALLENGE 🔥 <===
+
+#For this challenge, it's necessary to filter the dataframe.
+#The filtering should satisfie two requirements:
+#=> Only RIO DE JANEIRO's gas station.
+#=> PREÇO MÉDIO DE REVENDA should be above R$2.
+
+#Each way of filtering is unique. Only one filter per filter.
+#If you want multiple filters at once, use "(FILTER) & (FILTER)".
+""" selection = (dataframe["ESTADO"] == "RIO DE JANEIRO") & ~(dataframe["PREÇO MÉDIO REVENDA"] > 2);
+
+filtered_df = dataframe[selection].reset_index(drop=True);
+print(filtered_df); """
+
+#===> CHALLENGE END 🏁 <===
+
+#OPERATORS AT FILTERING 💡]
+
+#While filtering, it's necessary, as seen before, to use conditions during the process of filtering.
+#For this, there are OPERATORS (&, |, ~) for helping creating logics of conditions.
+#The operator "&" means for "AND".
+#The operator "|" means for "OR".
+#The operator "~" means for "NOT".
+
+#==> AND <==
+#Here, only registers where the ESTADO is equal to SERGIPE AND PREÇO MÉDIO REVENDA is greater than 2 'll be selected.
+""" aditive_selection = (dataframe["ESTADO"] == "SERGIPE") & (dataframe["PREÇO MÉDIO REVENDA"] >= 2);
+filtered_df = dataframe[aditive_selection];
+print(filtered_df.iloc[0]) """
+
+#==> OR <==
+#Here, only registers where the REGIÃO is equal to SUDESTE OR to NORDESTE 'll be selected.
+""" alternative_selection = (dataframe["REGIÃO"] == "SUDESTE") | (dataframe["REGIÃO"] == "NORDESTE");
+filtered_df = dataframe[alternative_selection];
+print(filtered_df["REGIÃO"].unique()); """
+
+#==> NOT <==
+#Here, only the registers where the REGIÃO is NOT equal to SUDESTE AND NOT equal to NORDESTE 'll be selected.
+""" adversative_selection = ~(dataframe["REGIÃO"] == "SUDESTE") & ~(dataframe["REGIÃO"] == "NORDESTE");
+filtered_df = dataframe[adversative_selection];
+print(filtered_df["REGIÃO"].unique()); """
